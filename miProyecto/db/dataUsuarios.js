7357cca124d9d8edf7872e9da5f2534d4aa5529d
catagoyanarte <cata.goyanarte@icloud.com>
2025-04-01 19:44:59 -0300

    agregue modulo3 ej 1

---
+++ miProyecto/db/dataUsuarios.js
@@ -0,0 +1,11 @@
+const usuarioData = {
+    usuario: {
+        email: "orianasabatini@gmai.com",
+        usuario: "ori_sabatini",
+        contrasenia: "dybala",
+        fechaNacimiento: "14-01-1999",
+        numeroDocumento: "47090878",
+        fotoPerfil: '/images/users/oriana.jpg'
+    }
+};
+
