c1a8dae74642a52a5ddfd72ed4ca224574d90ffb
manuelajudez <manuelajudezrivas@yahoo.com>
2025-04-02 20:57:05 -0300

    termine el objeto literal de productos, y el detalle de dichos en partials

--- miProyecto/db/products.js
+++ miProyecto/db/products.js
@@ -4,61 +4,131 @@ const productos = {
         id: 1,
         imagen: "/images/products/libro1.jpg", 
         nombre: "Pride and Practice",
-        descripcion: "Romance contemporáneo, orgullo, prejuicios, identidad, amor, familia, crecimiento, conflicto, diversidad."
+        descripcion: "Romance contemporáneo, orgullo, prejuicios, identidad, amor, familia, crecimiento, conflicto, diversidad.",
+        comentarios: [
+            {
+                usuario: "CataGoyanarte", 
+                comentario: "Excelente servicio y me encantó el libro que compré.",
+                fotoPerfil: "/images/comentarios/CGoyanarte.jpg"
+            },   
+        ]
     },
     {
         id: 2,
         imagen: "/images/products/libro2.jpg", 
         nombre: "Alls fair in love and poetry",
-        descripcion: "Éxito musical, evolución artística, empoderamiento, fama, letras, giras, impacto, fans."
+        descripcion: "Éxito musical, evolución artística, empoderamiento, fama, letras, giras, impacto, fans.",
+        comentarios: [
+            {
+                usuario: "ManuJudez", 
+                comentario: "La entrega fue rápida y el libro llegó en perfectas condiciones.",
+                fotoPerfil: "/images/comentarios/MJudez.jpg"
+            },   
+        ]
     },
     {
         id: 3,
         imagen: "/images/products/libro3.jpg", 
         nombre: "Frankestein",
-        descripcion: "Ciencia, ambición, creación, monstruo, culpa, soledad, rechazo, venganza, horror, humanidad."
+        descripcion: "Ciencia, ambición, creación, monstruo, culpa, soledad, rechazo, venganza, horror, humanidad.",
+        comentarios: [
+            {
+                usuario: "MiliGalarce", 
+                comentario: "¡Recomendado!",
+                fotoPerfil: "/images/comentarios/MGalarce.jpg"
+            },
+        ]
     },
     {
         id: 4,
         imagen: "/images/products/libro4.jpg", 
         nombre: "Little Woman",
-        descripcion: "Hermanas, crecimiento, familia, amor, sueños, guerra, arte, sacrificio, independencia, esperanza."
+        descripcion: "Hermanas, crecimiento, familia, amor, sueños, guerra, arte, sacrificio, independencia, esperanza.",
+        comentarios: [
+            {
+                usuario: "ORodrigo", 
+                comentario: "Tardó un poco en llegar. Aún así, vale la pena.",
+                fotoPerfil: "/images/comentarios/ORodrigo.jpg"
+            }, 
+        ]
     },
     {
         id: 5,
         imagen: "/images/products/libro5.jpg", 
         nombre: "Letters of Jane Austin",
-        descripcion: "Correspondencia, intimidad, observación, ingenio, sociedad, familia, escritura, emociones, rutina, época."
+        descripcion: "Correspondencia, intimidad, observación, ingenio, sociedad, familia, escritura, emociones, rutina, época.",
+        comentarios: [
+            {
+                usuario: "AdamPort", 
+                comentario: "Gran selección de títulos para adolescentes.",
+                fotoPerfil: "/images/comentarios/APort.jpg"
+            },
+        ]
     },
     {
         id: 6,
         imagen: "/images/products/libro6.jpg", 
         nombre: "The Romance of Royalty",
-        descripcion: "Amor, poder, deber, escándalo, tradiciones, pasiones, secretos, nobleza, alianzas, destino."
+        descripcion: "Amor, poder, deber, escándalo, tradiciones, pasiones, secretos, nobleza, alianzas, destino.",
+        comentarios: [
+            {
+                usuario: "GracieAbrams", 
+                comentario: "Estoy muy feliz con mi compra.",
+                fotoPerfil: "/images/comentarios/GAbrams.jpg"
+            },
+        ]
     },
     {
         id: 7,
         imagen: "/images/products/libro7.jpg", 
         nombre: "The Secret Garden",
-        descripcion: "Jardín, amistad, sanación, descubrimiento, naturaleza, infancia, pérdida, esperanza, magia, transformación."
+        descripcion: "Jardín, amistad, sanación, descubrimiento, naturaleza, infancia, pérdida, esperanza, magia, transformación.",
+        comentarios: [
+            {
+                usuario: "Rampa", 
+                comentario: "Perfecto para mantener el interés de los lectores jóvenes.",
+                fotoPerfil: "/images/comentarios/Rampa.jpg"
+            },
+        ]
     },
     {
         id: 8,
         imagen: "/images/products/libro8.jpg", 
         nombre: "Love finds the way",
-        descripcion: "Amor, obstáculos, coraje, superación, destino, esperanza, sacrificio, pasión, crecimiento, redención."
+        descripcion: "Amor, obstáculos, coraje, superación, destino, esperanza, sacrificio, pasión, crecimiento, redención.",
+        comentarios: [
+            {
+                usuario: "ADiaz", 
+                comentario: "Sin duda, volveré a comprar aquí.",
+                fotoPerfil: "/images/comentarios/ADiaz.jpg"
+            },
+        ]
     },
     {
         id: 9,
         imagen: "/images/products/libro9.jpg", 
         nombre: "Fairytales and Stories",
-        descripcion: "Magia, fantasía, lecciones, aventuras, criaturas, sueños, inocencia, misterio, valores, imaginación."
+        descripcion: "Magia, fantasía, lecciones, aventuras, criaturas, sueños, inocencia, misterio, valores, imaginación.",
+        comentarios: [
+            {
+                usuario: "Miloj", 
+                comentario: "Un libro que no solo entretiene sino que también educa. Ideal para adolescentes.",
+                fotoPerfil: "/images/comentarios/MiloJ.jpg"
+            },
+        ]
     },
     {
         id: 10,
         imagen: "/images/products/libro10.jpg", 
         nombre: "The Bird",
-        descripcion: "Libertad, vuelo, naturaleza, canto, belleza, símbolo, soledad, instinto, paz, observación."
+        descripcion: "Libertad, vuelo, naturaleza, canto, belleza, símbolo, soledad, instinto, paz, observación.",
+        comentarios: [
+            {
+                usuario: "KendrickL", 
+                comentario: "¡Excelente compra!",
+                fotoPerfil: "/images/comentarios/KLamar.jpg"
+            },
+        ]
     }
         ] 
             };
