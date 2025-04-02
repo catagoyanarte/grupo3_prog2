989660d5284d67c596424fc375a0bbfdca1388a0
manuelajudez <manuelajudezrivas@yahoo.com>
2025-04-02 13:57:40 -0300

    agrego los arrays de objetos literales necesarios

---
+++ miProyecto/db/comments.js
@@ -0,0 +1,74 @@
+const comentarios = [
+    {
+        id: 1,
+        usuario: "CataGoyanarte", 
+        comentario: "",
+        fotoPerfil: ""
+    },   
+     {
+        id: 2,
+        usuario: "ManuJudez", 
+        comentario: "",
+        fotoPerfil: ""
+    },   
+     {
+        id: 3,
+        usuario: "MiliGalarce", 
+        comentario: "",
+        fotoPerfil: ""
+    },
+    {
+        id: 4,
+        usuario: "ORodrigo", 
+        comentario: "",
+        fotoPerfil: ""
+    },
+    {
+        id: 5,
+        usuario: "AdamPort", 
+        comentario: "",
+        fotoPerfil: ""
+    },
+    {
+        id: 6,
+        usuario: "", 
+        comentario: "",
+        fotoPerfil: ""
+    },
+    {
+        id: 7,
+        usuario: "", 
+        comentario: "",
+        fotoPerfil: ""
+    },
+    {
+        id: 8,
+        usuario: "", 
+        comentario: "",
+        fotoPerfil: ""
+    },
+    {
+        id: 9,
+        usuario: "", 
+        comentario: "",
+        fotoPerfil: ""
+    },
+    {
+        id: 10,
+        usuario: "", 
+        comentario: "",
+        fotoPerfil: ""
+    },
+    {
+        id: 11,
+        usuario: "", 
+        comentario: "",
+        fotoPerfil: ""
+    },
+    {
+        id: 12,
+        usuario: "", 
+        comentario: "",
+        fotoPerfil: ""
+    },
+] 
---
+++ miProyecto/db/products.js
@@ -0,0 +1,62 @@
+const productos = [
+    {
+        id: 1,
+        imagen: "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/7b539535-2421-4ea8-84e2-43399865843e/d_295_510/portada_dioses-del-juego-1-the-striker_ana-huang_202411281205.webp", 
+        nombre: "The Striker",
+        descripcion: "Este libro de Ana Huang es la fusion perfecta entre el deporte y el romance.."
+    },
+    {
+        id: 2,
+        imagen: "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/eea83e8a-5ba2-4830-9593-25a3453f3917/d_295_510/portada_todo-lo-que-nunca-fuimos_alice-kellen_201910311009.webp", 
+        nombre: "Todo lo que nunca fuimos",
+        descripcion: "Porque a veces basta con un «deja que ocurra» para arriesgarlo todo. Alice Kellen es la autora más vendida en edición de bolsillo en 2021 y 2022."
+    },
+    {
+        id: 3,
+        imagen: "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/e85f1df4-68a9-4aa4-97ab-bd27292de779/d_295_510/portada_twisted-2-twisted-games_ana-huang_202211021750.webp", 
+        nombre: "Twisted Games",
+        descripcion: "Este libro de la saga Twisted, de Ana Huang, te va a causar una montaña rusa de emociones!"
+    },
+    {
+        id: 4,
+        imagen: "https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/c7d7198a-b0d7-45f6-8712-1bc050347622/d_295_510/portada_heartstopper-4-mas-que-palabras_victoria-simo-perales_202103251448.webp", 
+        nombre: "Heartstopper",
+        descripcion: "¿Quién no se ha enamorado de Nick y Charlie? Los protagonistas de Heartstopper, sobre quienes se adapto la serie de Netflix que se ha convertido en todo un fenómeno."
+    },
+    {
+        id: 5,
+        imagen: "https://www.educaciontrespuntocero.com/wp-content/uploads/2024/10/Redes-1.jpg", 
+        nombre: "Redes",
+        descripcion: "¿Cuántos likes vale tu felicidad? La última novela de Eloy Moreno continúa el universo de ‘Invisible’, una historia valiente y emotiva sobre acoso escolar."
+    },
+    {
+        id: 6,
+        imagen: "https://www.educaciontrespuntocero.com/wp-content/uploads/2022/07/el-mar-detras.jpg", 
+        nombre: "El mar detrás",
+        descripcion: "Ganador del Premio Gran Angular 2022, en este relato la joven Isata vive en un campo de refugiados junto a su amiga Dibra."
+    },
+    {
+        id: 7,
+        imagen: "https://www.educaciontrespuntocero.com/wp-content/uploads/2022/06/Historias-De-Astronomia.jpg", 
+        nombre: "Historias de astronomía.",
+        descripcion: "Esta novela juvenil de la escritora danesa Gertrude Kiel tiene como protagonista a William, un niño muy curioso que quiere descubrir los misterios que habitan el universo."
+    },
+    {
+        id: 8,
+        imagen: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1741175780-81oXooCvJL.jpg?crop=1xw:0.986xh;center,top&resize=640:*", 
+        nombre: "Bajo la misma estrella",
+        descripcion: "Una conmovedora historia que sigue a Hazel Grace Lancaster, una adolescente con cáncer de pulmón que, pese a su enfermedad, intenta y quiere vivir una vida normal."
+    },
+    {
+        id: 9,
+        imagen: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1704984574-81Y3gTKbzsL.jpg?crop=1xw:0.981xh;center,top&resize=980:*", 
+        nombre: "Wonder",
+        descripcion: "August Pullman es un niño con el síndrome de Treacher Collins, y debera enfrentarse por primera vez a la crueldad infantil."
+    },
+    {
+        id: 10,
+        imagen: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1685199200-71rDyMBfBoL.jpg?crop=1xw:0.987xh;center,top&resize=980:*", 
+        nombre: "Invisible",
+        descripcion: "'Invisible' nos cuenta la historia de un niño que se siente rechazado y se convence de que es invisible para los demás."
+    }
+];
