a4461f1f34af25d199e27a73ce1d1b7f80156dba
MilagrosGalarce <galarcem@udesa.edu.ar>
2025-05-16 12:37:32 -0300

    Resuelto conflicto en productController.js

--- miProyecto/controllers/productController.js
+++ miProyecto/controllers/productController.js
@@ -5,9 +5,7 @@ const productoController = {
     let producto = products.producto[0];
     res.render('producto', {producto});
   }
-<<<<<<< HEAD
-};p
-=======
 };
->>>>>>> bb2311ace492569e0715b2baf2174a47be67b40d
+
+
 module.exports = productoController;
