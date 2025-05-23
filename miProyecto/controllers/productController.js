f4023b6c8f71f836cb06b9f5d3e52138dc6bab57
MilagrosGalarce <galarcem@udesa.edu.ar>
2025-05-23 14:08:04 -0300

    implemente el controlador producto con sequelize

--- miProyecto/controllers/productController.js
+++ miProyecto/controllers/productController.js
@@ -1,12 +1,23 @@
 //const products = require('../db/products');
-const db = require('../database/models');
+//const db = require('../database/models');
+
 
 const productoController = {
-  detalle: function (req,res) {
-    let producto = products.producto[0];
-    res.render("producto", {producto});
+  detalle: function (req, res) {
+
+    Producto.findAll()
+      .then(function (productos) {
+        let producto = productos[0];
+        res.render("producto", { producto });
+      })
+      .catch(function (error) {
+        return res.send(error);
+      })
   }
 };
+
+
+
 
 
 module.exports = productoController;
