666c88edd0f5974f97514fea70082430ed8508ca
manuelajudez <manuelajudezrivas@yahoo.com>
2025-05-26 11:02:10 -0300

    validaciones de login

--- miProyecto/controllers/userController.js
+++ miProyecto/controllers/userController.js
@@ -28,18 +28,20 @@ const userController = {
 
     }
 
-    if (email =="") {
+    if (email == "") {
       error.email = "Email obligatorio";
-            return res.render("register", { error });
+      return res.render("register", { error });
 
     }
 
-    if (contrasena =="" || contrasena.length < 3) {
+    if (contrasena == "" || contrasena.length < 3) {
       error.contrasena = "La contraseña debe tener al menos 3 caracteres";
+      return res.render("register", { error });
     }
 
-    if (fecha_nacimiento =="") {
+    if (fecha_nacimiento == "") {
       error.fecha_nacimiento = "La fecha de nacimiento es obligatoria";
+      return res.render("register", { error });
     }
 
     db.Usuario.findOne({ where: { email: email } })
@@ -80,7 +82,7 @@ const userController = {
     let email = req.body.email;
     let contrasena = req.body.contrasena;
     let recordarme = req.body.recordarme;
-    let error_login = {};
+    let error = {};
 
     let userInfo = {
       email: email,
@@ -89,19 +91,21 @@ const userController = {
     }
 
     // valido que email y contrasena sean correctas
-    
+
     db.Usuario.findOne({ where: { email: email } })
-    .then(function(user) {
-      if (user) {
-        error_login.email = "Este email no ha sido registrado";
-        return res.render("login", { error_login: {} })}
-    });
+      .then(function (user) {
+        if (user) {
+          error.email = "Este email no ha sido registrado";
+          return res.render("login", { error: {} })
+        }
+      });
 
     let validacion = bcrypt.compareSync(req.body.contrasena, contrasena);
-        if (!validacion) {
-          req.session.user = contrasena;
-          error_login.contrasena = "Esta contrasena no es valida";
-        return res.render("login", { error_login: {} })}
+    if (validacion == "") {
+      req.session.user = contrasena;
+      error.contrasena = "Esta contrasena no es valida";
+      return res.render("login", { error: {} })
+    }
 
     //poner al usuario en session
     req.session.user = userInfo;
@@ -111,21 +115,32 @@ const userController = {
       res.cookie("user", userInfo, { maxAge: 150000 });
     }
 
-    res.redirect("/perfil")
+    res.redirect("/profile")
 
+  },
+
+  logout: function (req, res) {
+    req.session.destroy();
+    res.clearCookie("user");
+    return res.redirect("/")
+  },
+
+  profile: function (req, res) {
+    let nombreUsuario = users.usuario;
+    let productos = products.producto;
+    res.render('perfil', { user: nombreUsuario, productos: productos });
   }
-  // req.session.destroy();   --> log out 
 };
 
 
-    // if (userInfo.email !== email) {
-    //   error.email = "Este mail no ha sido registrado.";
-    // } else {
- 
-    // }
-    // if (userInfo.contrasena !== contrasena) {
-    //   error.email = "La contrasena no corresponde al mail registrado.";
-    // } else {
+// if (userInfo.email !== email) {
+//   error.email = "Este mail no ha sido registrado.";
+// } else {
+
+// }
+// if (userInfo.contrasena !== contrasena) {
+//   error.email = "La contrasena no corresponde al mail registrado.";
+// } else {
 
 /*
 const userController = {
--- miProyecto/routes/userRoutes.js
+++ miProyecto/routes/userRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/register', userController.show);
-router.post('/newuser', userController.create)
+router.post('/profile', userController.create)
 router.get('/login', userController.showLogin);
 router.post('/login', userController.createLogin);
-//router.get('/profile/:id?', userController.profile);
+router.get('/profile/:id?', userController.profile);
 
 module.exports = router;
